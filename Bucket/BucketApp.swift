96fed4e3479b4bfd7ba7b5a743135d9c414327d4
Nicholas Runje <nickrunje@gmail.com>
2023-04-25 20:26:32 -0400

    First commit

---
+++ Bucket/BucketApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketApp.swift
+//  Bucket
+//
+//  Created by Nicholas Runje on 4/25/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
